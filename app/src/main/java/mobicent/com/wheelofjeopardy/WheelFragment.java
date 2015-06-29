aeb79ff83f5372768690615cde348e6128d6d48e
zkjellberg <zach.kjellberg@gmail.com>
2015-06-28 21:52:13 -0400

    Quick code cleanup.

--- app/src/main/java/mobicent/com/wheelofjeopardy/WheelFragment.java
+++ app/src/main/java/mobicent/com/wheelofjeopardy/WheelFragment.java
@@ -26,54 +26,49 @@ public class WheelFragment extends Fragment {
 
         fortuneView = (FortuneView) v.findViewById(R.id.dialView);
 
-        ArrayList<FortuneItem> dis = new ArrayList<>();
-        /*
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_always_landscape_portrait)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_add)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_agenda)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_camera)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_compass)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_help)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_mapmode)));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_save)));
-        */
+        ArrayList<FortuneItem> sectors = new ArrayList<>();
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_always_landscape_portrait)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_add)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_agenda)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_camera)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_compass)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_help)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_mapmode)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_save)));
 
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_always_landscape_portrait), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_add), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_agenda), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_camera), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_compass), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_help), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_mapmode), FortuneItem.HingeType.Fixed));
-        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_save), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_always_landscape_portrait), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_add), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_agenda), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_camera), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_compass), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_help), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_mapmode), FortuneItem.HingeType.Fixed));
+        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_save), FortuneItem.HingeType.Fixed));
 
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_0)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_1)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_2)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_3)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_4)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_5)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_6)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_7)));
-//        dis.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_8)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_0)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_1)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_2)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_3)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_4)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_5)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_6)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_7)));
+//        sectors.add(new FortuneItem(BitmapFactory.decodeResource(getResources(), R.drawable.image_8)));
 
-        /* These do not work
-        dis.add(new FortuneItem(Color.BLACK, 1));
-        dis.add(new FortuneItem(Color.BLUE, 1));
-        dis.add(new FortuneItem(Color.RED, 1));
-        dis.add(new FortuneItem(Color.GREEN, 1));
-        dis.add(new FortuneItem(Color.MAGENTA, 1));
-        */
+        // These do not work
+//        sectors.add(new FortuneItem(Color.BLACK, 1));
+//        sectors.add(new FortuneItem(Color.BLUE, 1));
+//        sectors.add(new FortuneItem(Color.RED, 1));
+//        sectors.add(new FortuneItem(Color.GREEN, 1));
+//        sectors.add(new FortuneItem(Color.MAGENTA, 1));
 
-        fortuneView.addFortuneItems(dis);
+        fortuneView.addFortuneItems(sectors);
 
         v.findViewById(R.id.btRandom).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Random ran = new Random();
                 int randomInt = ran.nextInt(fortuneView.getTotalItems());
-                //fortuneView.setSelectedItem((fortuneView.getSelectedIndex() == fortuneView.getTotalItems() - 1 ? 0 : fortuneView.getSelectedIndex() + 1));
-                //fortuneView.setSelectedItem((fortuneView.getSelectedIndex() == 0 ? fortuneView.getTotalItems() - 1 : fortuneView.getSelectedIndex() - 1));
                 Toast.makeText(getActivity(),""+randomInt,Toast.LENGTH_SHORT).show();
                 fortuneView.setSelectedItem(randomInt);
             }
