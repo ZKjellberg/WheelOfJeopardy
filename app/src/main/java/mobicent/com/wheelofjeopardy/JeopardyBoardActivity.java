67020ee768441ef13485e4c6728f36acddd9d9c9
Gregory Kuruc <gkuruc@silentcircle.com>
2015-07-03 20:26:14 -0400

    Add a Jeopardy Board

--- app/src/main/java/mobicent/com/wheelofjeopardy/JeopardyBoardActivity.java
+++ app/src/main/java/mobicent/com/wheelofjeopardy/JeopardyBoardActivity.java
@@ -1,28 +1,24 @@
 package mobicent.com.wheelofjeopardy;
 
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class JeopardyBoardActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, JeopardyBoardActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_jeopardy_board);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_jeopardy_board, menu);
         return true;
     }
 
