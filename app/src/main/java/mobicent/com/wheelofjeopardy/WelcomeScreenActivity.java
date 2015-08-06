0741095d382ab691382dd3ce21aba01da889e5ac
zkjellberg <zach.kjellberg@gmail.com>
2015-08-05 20:54:41 -0400

    Minor tweaks.

--- app/src/main/java/mobicent/com/wheelofjeopardy/WelcomeScreenActivity.java
+++ app/src/main/java/mobicent/com/wheelofjeopardy/WelcomeScreenActivity.java
@@ -3,6 +3,7 @@ package mobicent.com.wheelofjeopardy;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
-public class WelcomeScreenActivity extends ActionBarActivity {
+public class WelcomeScreenActivity extends AppCompatActivity {
 
     Button startButton;
     EditText numPlayersEditText;
